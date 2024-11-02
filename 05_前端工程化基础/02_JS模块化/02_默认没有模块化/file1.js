82ac1a26386eb18c03e8559361121d0f04b88729
ineyee <ineyee@foxmail.com>
2024-11-02 12:17:00 +0800

    前端工程化基础：02_默认没有模块化

---
+++ 05_前端工程化基础/02_JS模块化/02_默认没有模块化/file1.js
@@ -0,0 +1,6 @@
+// 全局作用域
+const personName = "张三";
+
+function run() {
+  console.log(`${personName} run`);
+}
---
+++ 05_前端工程化基础/02_JS模块化/02_默认没有模块化/file2.js
@@ -0,0 +1,6 @@
+// 全局作用域
+const personName = "李四";
+
+function run() {
+  console.log(`${personName} run`);
+}
