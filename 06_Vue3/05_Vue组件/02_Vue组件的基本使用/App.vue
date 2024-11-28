445eb7ac391309971228fb0fb14100b1c7504d84
ineyee <ineyee@foxmail.com>
2024-11-28 15:35:15 +0800

    Vue组件：Vue组件的基本使用

---
+++ 06_Vue3/05_Vue组件/02_Vue组件的基本使用/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <hello-world></hello-world>
+  <HelloWorld></HelloWorld>
+</template>
+
+<script>
+import HelloWorld from "./HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+}
+</script>
+
+<style scoped></style>
