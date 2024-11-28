0ed2a326a0c7b27e8fac26e8eba585497ccc70b6
ineyee <ineyee@foxmail.com>
2024-11-28 15:34:54 +0800

    Vue组件：Vue组件是什么

---
+++ 06_Vue3/05_Vue组件/01_Vue组件是什么/CounterComponent.vue
@@ -0,0 +1,36 @@
+<!-- 比如这就是个有状态Component -->
+
+<!-- UI（即视图）的结构 -->
+<template>
+  <div class="counter">当前计数: {{ counter }}</div>
+  <button @click="add">➕</button>
+</template>
+
+<script>
+export default {
+  // 组件的名称
+  name: "CounterComponent",
+
+  // 组件的状态（即数据）
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+
+  // 组件的交互（即事件）
+  methods: {
+    add() {
+      this.counter++;
+    },
+  },
+}
+</script>
+
+<!-- UI（即视图）的样式 -->
+<style scoped>
+.counter {
+  font-size: 20px;
+  font-weight: 500,
+}
+</style>
---
+++ 06_Vue3/05_Vue组件/01_Vue组件是什么/SeperateLineComponent.vue
@@ -0,0 +1,21 @@
+<!-- 比如这就是个无状态Component -->
+
+<!-- UI（即视图）的结构 -->
+<template>
+  <div class="line"></div>
+</template>
+
+<script>
+export default {
+  // 组件的名称
+  name: "SeperateLineComponent",
+}
+</script>
+
+<!-- UI（即视图）的样式 -->
+<style scoped>
+.line {
+  height: 1px;
+  background-color: black;
+}
+</style>
