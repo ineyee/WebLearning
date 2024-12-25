71a24e4ef7aab8dc98c268cd918890546414cdbb
ineyee <ineyee@foxmail.com>
2024-12-25 09:26:05 +0800

    fun-trip: 首页——导航栏

---
+++ 06_Vue3/项目实战/fun-trip/src/component/nav-bar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="nav-bar">
+    <van-nav-bar title="趣旅行" />
+  </div>
+</template>
+
+<script setup>
+defineOptions({
+  name: "nav-bar"
+});
+</script>
+
+<style lang="less" scoped>
+.nav-bar {
+  :deep(.van-nav-bar__title) {
+    font-size: var(--fs-20);
+    font-weight: var(--fw-medium);
+    color: var(--main-color);
+  }
+}
+</style>
--- 06_Vue3/项目实战/fun-trip/src/home/page/home-page.vue
+++ 06_Vue3/项目实战/fun-trip/src/home/page/home-page.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="home-page">
-    home-page
+    <nav-bar></nav-bar>
   </div>
 </template>
 
 <script setup>
+import NavBar from '../../component/nav-bar.vue';
+
 defineOptions({
   name: "home-page"
 });
