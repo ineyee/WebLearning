2dad7bd4618521e4d36ab081923e5b039ac9c202
ineyee <ineyee@foxmail.com>
2024-12-25 10:47:05 +0800

    fun-trip: 首页——城市

---
+++ 06_Vue3/项目实战/fun-trip/src/home/component/city-component.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="city-component">
+    <div class="city">杭州</div>
+    <div class="position">
+      <div class="my-position">我的位置</div>
+      <img class="position-icon" src="../../asset/image/icon_location.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineOptions({
+  name: "city-component"
+});
+</script>
+
+<style lang="less" scoped>
+.city-component {
+  padding: 10px 20px 10px 20px;
+
+  display: flex;
+  justify-content: space-between;
+
+  .position {
+    display: flex;
+    align-items: center;
+
+    .position-icon {
+      width: 18px;
+      height: 18px;
+      padding-left: 10px;
+    }
+  }
+}
+</style>
--- 06_Vue3/项目实战/fun-trip/src/home/page/home-page.vue
+++ 06_Vue3/项目实战/fun-trip/src/home/page/home-page.vue
@@ -2,12 +2,14 @@
   <div class="home-page">
     <nav-bar></nav-bar>
     <banner-component></banner-component>
+    <city-component></city-component>
   </div>
 </template>
 
 <script setup>
 import NavBar from '../../component/nav-bar.vue';
 import BannerComponent from '../component/banner-component.vue';
+import CityComponent from '../component/city-component.vue';
 
 defineOptions({
   name: "home-page"
