14fa5507c31dac4b36e9d25cd1f7b4968c38c554
ineyee <ineyee@foxmail.com>
2025-01-02 15:37:15 +0800

    fun-trip: tabBar开启路由模式

--- 06_Vue3/项目实战/fun-trip/src/tab-bar/tab-bar.vue
+++ 06_Vue3/项目实战/fun-trip/src/tab-bar/tab-bar.vue
@@ -4,7 +4,7 @@
       我们可以直接在template模板里使用vant里的组件，连导入都不用写，插件会自动解析template模板并帮我们自动导入所用到的组件
     -->
     <van-tabbar v-model="selectedIndex" v-show="curShowRoute.meta.showTabBar" active-color="#FF9854FF"
-      inactive-color="#999999FF" :safe-area-inset-bottom="true">
+      inactive-color="#999999FF" :safe-area-inset-bottom="true" route>
       <van-tabbar-item icon="home-o" to="/home">首页</van-tabbar-item>
       <van-tabbar-item icon="like-o" to="/favor">收藏</van-tabbar-item>
       <van-tabbar-item icon="cart-o" to="/order">订单</van-tabbar-item>
