25913779dde76e533d69f7805e4bbadb4787b0d3
ineyee <ineyee@foxmail.com>
2025-01-03 18:20:47 +0800

    fun-trip: keep-alive tabBar的几个直接子界面

--- 06_Vue3/项目实战/fun-trip/src/App.vue
+++ 06_Vue3/项目实战/fun-trip/src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="app">
-    <!-- 使用router-view元素占位，这样将来我们切换路由时，路由对应的组件就会替换出现在router-view元素的位置 -->
-    <router-view></router-view>
+    <!--
+      使用router-view元素占位，这样将来我们切换路由时，路由对应的组件就会替换出现在router-view元素的位置
+      router-view做keep-alive时的固定写法加个v-slot
+    -->
+    <router-view v-slot="props">
+      <!--
+        使用keep-alive把tabBar的几个直接界面给保活
+        include里对应的就是界面的name属性值，多个界面的用逗号隔开
+      -->
+      <keep-alive include="home-page, favor-page, order-page, message-page">
+        <component :is="props.Component"></component>
+      </keep-alive>
+    </router-view>
     <tab-bar></tab-bar>
     <loading-component v-show="_commonStore.isShowLoading"></loading-component>
   </div>
