5462b3774e62e1947eded58766e682802c0e5ec3
ineyee <ineyee@foxmail.com>
2025-01-15 12:47:44 +0800

    小程序开发：02_小程序项目目录结构：某个界面.json文件和某个界面.wxss文件

--- 07_小程序开发/02_小程序项目目录结构/pages/logs/logs.js
+++ 07_小程序开发/02_小程序项目目录结构/pages/logs/logs.js
@@ -2,17 +2,33 @@
 const util = require('../../utils/util.js')
 
 Page({
-  data: {
-    logs: []
-  },
-  onLoad() {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return {
-          date: util.formatTime(new Date(log)),
-          timeStamp: log
-        }
-      })
-    })
-  }
+	data: {
+		logs: []
+	},
+	onLoad() {
+		this.setData({
+			logs: (wx.getStorageSync('logs') || []).map(log => {
+				return {
+					date: util.formatTime(new Date(log)),
+					timeStamp: log
+				}
+			})
+		})
+	},
+
+	// 监听下拉刷新
+	onPullDownRefresh() {
+		console.log("下拉刷新");
+
+		// 模拟网络请求
+		setTimeout(() => {
+			console.log("消失下拉刷新");
+			wx.stopPullDownRefresh();
+		}, 10000);
+	},
+
+	// 监听界面滚动到底部
+	onReachBottom() {
+		console.log("界面滚动到底部");
+	}
 })
