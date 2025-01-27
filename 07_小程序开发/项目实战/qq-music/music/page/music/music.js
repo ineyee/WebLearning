04c248f0e49fe0769940f377560af825e4a9c89e
ineyee <ineyee@foxmail.com>
2025-01-27 17:02:24 +0800

    qq-music: 音乐首页轮播图

--- 07_小程序开发/项目实战/qq-music/music/page/music/music.js
+++ 07_小程序开发/项目实战/qq-music/music/page/music/music.js
@@ -1,7 +1,25 @@
+import { getBanner } from "../../service/music-service";
+
 Page({
+  data: {
+    bannerList: [],
+  },
+
+  onLoad() {
+    this._getBanner();
+  },
+
   onTapSearch() {
     wx.navigateTo({
       url: "../music-search/music-search",
     })
   },
+
+  // vm
+  async _getBanner() {
+    const data = await getBanner();
+    this.setData({
+      bannerList: data,
+    });
+  }
 })
---
+++ 07_小程序开发/项目实战/qq-music/music/service/music-service.js
@@ -0,0 +1,26 @@
+import httpRequest from '../../network/http-request';
+
+// 强调函数式编程，不写类，就像很多 hook 函数也是一样的直接写个函数导出出去了
+export async function getBanner() {
+  try {
+    const response = await httpRequest.get({
+      url: "http://codercba.com:9002/banner",
+    });
+    
+    if (response.data["code"] !== 200) {
+      console.log("/banner 请求失败：", response.data["code"]);
+      return [];
+    } else {
+      if (response.data["banners"] === null) {
+        console.log("/banner 请求失败：数据为 null");
+        return [];
+      } else {
+        console.log("/banner 请求成功：", response.data["banners"]);
+        return response.data["banners"];
+      }
+    }
+  } catch (error) {
+    console.log("/banner 请求失败：", error);
+    return [];
+  }
+};
