4abd0114282f4c06ccd2960fbd50133280e3d516
ineyee <ineyee@foxmail.com>
2025-02-01 16:20:35 +0800

    qq-music: 音乐播放界面自定义导航栏

--- 07_小程序开发/项目实战/qq-music/app.js
+++ 07_小程序开发/项目实战/qq-music/app.js
@@ -1,3 +1,22 @@
 // app.js
 App({
+  globalData: {
+    screenWidth: 375,
+    screenHeight: 812,
+    // 顶部安全区域高度，包含状态栏高度
+    safeAreaHeightTop: 44,
+    appBarHeight: 44,
+    navigationBarHeight: 44 + 44,
+    // 底部安全区域高度
+    safeAreaHeightBottom: 34,
+  },
+
+  onLaunch() {
+    const windowInfo = wx.getWindowInfo();
+    this.globalData.screenWidth = windowInfo.screenWidth;
+    this.globalData.screenHeight = windowInfo.screenHeight;
+    this.globalData.safeAreaHeightTop = windowInfo.safeArea.top;
+    this.globalData.navigationBarHeight = this.globalData.safeAreaHeightTop + this.globalData.appBarHeight;
+    this.globalData.safeAreaHeightBottom = this.globalData.screenHeight - windowInfo.safeArea.bottom;
+  },
 })
--- 07_小程序开发/项目实战/qq-music/music-play/page/music-play.js
+++ 07_小程序开发/项目实战/qq-music/music-play/page/music-play.js
@@ -1,16 +1,48 @@
 // music-play/page/music-play.js
-import { getSongDetail, getLyric} from "../service/music-play-servcie.js";
+import { getSongDetail, getLyric } from "../service/music-play-servcie.js";
 
 Page({
   data: {
+    statusBarHeight: 44,
+
     id: 0,
     songDetail: {},
     lyric: {},
+    selectedIndex: 0, // 0-歌曲，1-歌词
   },
 
   onLoad(options) {
+    const app = getApp();
+    this.setData({
+      statusBarHeight: app.globalData.safeAreaHeightTop,
+    });
+
     this._getSongDetail(options.id);
     this._getLyric(options.id);
+  },
+
+  onTapBack() {
+    wx.navigateBack();
+  },
+
+  onTapSong() {
+    if (this.data.selectedIndex === 0) {
+      return;
+    }
+
+    this.setData({
+      selectedIndex: 0,
+    });
+  },
+
+  onTapLyric() {
+    if (this.data.selectedIndex === 1) {
+      return;
+    }
+
+    this.setData({
+      selectedIndex: 1,
+    });
   },
 
   // vm
