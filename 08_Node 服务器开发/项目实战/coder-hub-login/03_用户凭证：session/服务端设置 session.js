564a4ff8de838f6fcf5e12373f9fbeeaf1d7af3a
ineyee <ineyee@foxmail.com>
2025-06-16 16:41:04 +0800

    feat: 登录凭证 session

---
+++ 08_Node 服务器开发/项目实战/coder-hub-login/03_用户凭证：session/服务端设置 session.js
@@ -0,0 +1,62 @@
+const Koa = require("koa");
+const KoaRouter = require("@koa/router");
+const koaSession = require("koa-session");
+
+const app = new Koa();
+app.listen(8000, () => {
+  console.log("🚀服务器启动成功🚀");
+});
+
+// 创建一个 session
+const session = koaSession(
+  {
+    // 其实就是浏览器里 cookie 的 key
+    key: "sessionId",
+    // 对 cookie 的值进行加盐
+    sign: true,
+    // 过期时间
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  },
+  app
+);
+// 盐值
+app.keys = ["111", "222", "333"];
+// 注册一下这个中间件，这样一来 ctx 里就有一个 session 对象了
+app.use(session);
+
+const router = new KoaRouter({ prefix: "/user" });
+router.get("/login", (ctx, next) => {
+  // 假设这里服务端判断到登录接口走成功...
+
+  /*
+    服务端给客户端设置 session
+      * session 名
+      * session 值
+      * session 过期时间（单位毫秒）
+  */
+  ctx.session.sessionId = "123456";
+
+  // 服务端给客户端响应数据
+  ctx.body = {
+    code: 0,
+    message: "登录成功",
+  };
+});
+router.get("/info", (ctx, next) => {
+  // 验证 sessionId
+  const sessionId = ctx.session.sessionId;
+  if (sessionId !== "123456") {
+    ctx.body = {
+      code: -2000,
+      message: "登录凭证无效",
+    };
+    return;
+  }
+
+  ctx.body = {
+    code: 0,
+    message: "获取用户信息成功",
+  };
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
