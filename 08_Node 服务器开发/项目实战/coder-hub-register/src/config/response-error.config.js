aa4a349599b1351bfcd1021adf6a1b64183ba4a6
ineyee <ineyee@foxmail.com>
2025-06-17 10:16:42 +0800

    coder-hub-register: 错误响应码调整

--- 08_Node 服务器开发/项目实战/coder-hub-register/src/config/response-error.config.js
+++ 08_Node 服务器开发/项目实战/coder-hub-register/src/config/response-error.config.js
@@ -1,28 +1,28 @@
 const responseError = {
-  // 注册模块错误码（-1001到-1999）
+  // 注册模块错误码（-1000到-1999）
   REGISTER: {
     EMAIL_IS_REQUIRED: {
-      code: -1001,
+      code: -1000,
       message: "邮箱不能为空",
     },
     USERNAME_IS_REQUIRED: {
-      code: -1002,
+      code: -1001,
       message: "用户名不能为空",
     },
     PASSWORD_IS_REQUIRED: {
-      code: -1003,
+      code: -1002,
       message: "密码不能为空",
     },
     USERNAME_OR_PASSWORD_IS_TOO_LONG: {
-      code: -1004,
+      code: -1003,
       message: "用户名或密码长度不能大于 100 个字符",
     },
     EMAIL_FORMAT_IS_INCORRECT: {
-      code: -1005,
+      code: -1004,
       message: "邮箱格式不正确",
     },
     EMAIL_IS_ALREADY_EXISTS: {
-      code: -1006,
+      code: -1005,
       message: "邮箱已被注册",
     },
   },
