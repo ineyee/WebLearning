441290eaf7cc9eb859587141f71f6d8c0cd04915
ineyee <ineyee@foxmail.com>
2025-06-20 13:39:19 +0800

    coder-hub-full: 项目部署到服务器

--- 08_Node 服务器开发/项目实战/coder-hub-full/src/config/database/database.config.js
+++ 08_Node 服务器开发/项目实战/coder-hub-full/src/config/database/database.config.js
@@ -1,8 +1,10 @@
 class DataBaseConfig {
+  // 这个无论是本地数据库还是远程数据库，都可以保持localhost，因为代码上传到服务器后，localhost 就是服务器的 IP 地址了
   static CONNECTION_HOST = "localhost";
   static CONNECTION_PORT = 3306;
   static CONNECTION_USER = "root";
-  static CONNECTION_PASSWORD = "Mysqlyiyi0202!";
+  // static CONNECTION_PASSWORD = "Mysqlyiyi0202!"; // 本地
+  static CONNECTION_PASSWORD = "Txyyiyi0202!"; // 远程
   static CONNECTION_DATABASE = "db_coder_hub";
   static CONNECTION_Limit = 10;
 }
--- 08_Node 服务器开发/项目实战/coder-hub-full/src/config/server/server.config.js
+++ 08_Node 服务器开发/项目实战/coder-hub-full/src/config/server/server.config.js
@@ -1,6 +1,7 @@
 class ServerConfig {
   // 部署到服务器时记得换成服务器的域名或 IP 地址，还有端口号
-  static SERVER_HOST = "http://localhost";
+  // static SERVER_HOST = "http://localhost"; // 本地
+  static SERVER_HOST = "http://118.25.70.197"; // 远程
   static SERVER_PORT = 8000;
 }
 
