86da2b0ba759617ea75480941467537e8b3357fc
ineyee <ineyee@foxmail.com>
2025-06-27 09:48:48 +0800

    10_TS: HelloTS

---
+++ 10_TS/00_概述/02_HelloTS.ts
@@ -0,0 +1,21 @@
+/*
+  注意：
+    * 这里用的是 boolean、number、string，而非 Boolean、Number、String，因为 TS 里的 boolean、number、string 对应 JS 里的原始类型 boolean、number、string
+    * 所以跟 JS 里一样，实际开发中我们应该总是使用原始类型而非包装类型，包装类型交由 JS 引擎自动去搞
+*/
+
+let a: boolean = true;
+console.log(a.valueOf());
+
+let b: number = 123;
+console.log(b.toString());
+
+let c: string = "Hello TS";
+console.log(c.length);
+
+/*
+  注意：
+    * 一个 ts 文件里如果没有 export 或 import，TS 会默认将此文件视为脚本文件而非模块文件，而脚本文件里的变量都是全局变量、会污染全局命名空间、会出现命名冲突等问题，模块文件就有自己独立的作用域、不会污染全局命名空间、不会出现命名冲突等问题
+    * 所以实际开发中我们应该添加 export {} 强制让文件成为模块
+*/
+export {};
